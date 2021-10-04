e7eb2dfed318c3e7c6d57960020d1908542ff0da
JS <51195305+sombra2@users.noreply.github.com>
2021-10-04 09:23:49 +0400

    alignment

--- smb.py
+++ smb.py
@@ -94,7 +94,7 @@ for i in range(0, len(tickers)):
         name_percentage = '+' + name_percentage
     else:
         name_percentage = name_percentage
-    data += '`{0:.<15} {1:>8.2f} | {2:>7} | {3:>6}% {4}`\n'.format(company,
+    data += '`{0:.<15} {1:>8.2f} | {2:>8} | {3:>6}% {4}`\n'.format(company,
                                                            name_today,
                                                            name_difference,
                                                            name_percentage,
